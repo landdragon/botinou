980651361c87b52d68882044c8ea6e5ad13da309
landdragon <guirado.maxime@gmail.com>
2020-12-04 15:31:01 +0100

    test2

--- bot/Gally.py
+++ bot/Gally.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
+import FunctionDict
+
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 HEROKU_RELEASE_VERSION = os.getenv("HEROKU_RELEASE_VERSION")
 HEROKU_RELEASE_CREATED_AT = os.getenv("HEROKU_RELEASE_CREATED_AT")
@@ -9,24 +11,7 @@ HEROKU_SLUG_DESCRIPTION = os.getenv("HEROKU_SLUG_DESCRIPTION")
 IsInNotProd = os.getenv("IsInNotProd")
 
 client = discord.Client()
-
-
-async def Epv(message, ListElementInMessage):
-    if len(ListElementInMessage) == 3 and ListElementInMessage[1].isnumeric() and ListElementInMessage[2].isnumeric():
-        a = int(ListElementInMessage[1])
-        b = int(ListElementInMessage[2])
-        c = round(a/(1-(b/(b+1200))))
-        embed = discord.Embed(title="Epv", color=0xffffff)
-        embed.set_thumbnail(url="")
-        embed.add_field(name="Calculs effectués avec:", value="\nPV : " +
-                        str(a) + "\nDéfense : " + str(b) + "\n\nEpv : " + str(c), inline=False)
-        await message.channel.send(embed=embed)
-    else:
-        await message.channel.send("Erreur, Ecrivez Epv suivi des PV et de la Def. (ex : Epv 20000 2000)")
-
-Functions = {
-    "Epv2": Epv
-}
+Functions = FunctionDict.Functions
 
 
 @client.event
