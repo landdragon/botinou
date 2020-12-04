fe2e022e6f79e16581fa4d721359c28fc7f6119d
landdragon <guirado.maxime@gmail.com>
2020-12-04 15:51:49 +0100

    test3

--- bot/FunctionDict.py
+++ bot/FunctionDict.py
@@ -1,19 +1,8 @@
 import discord
-
-
-async def Epv(message, ListElementInMessage):
-    if len(ListElementInMessage) == 3 and ListElementInMessage[1].isnumeric() and ListElementInMessage[2].isnumeric():
-        a = int(ListElementInMessage[1])
-        b = int(ListElementInMessage[2])
-        c = round(a/(1-(b/(b+1200))))
-        embed = discord.Embed(title="Epv", color=0xffffff)
-        embed.set_thumbnail(url="")
-        embed.add_field(name="Calculs effectués avec:", value="\nPV : " +
-                        str(a) + "\nDéfense : " + str(b) + "\n\nEpv : " + str(c), inline=False)
-        await message.channel.send(embed=embed)
-    else:
-        await message.channel.send("Erreur, Ecrivez Epv suivi des PV et de la Def. (ex : Epv 20000 2000)")
+import Function
 
 Functions = {
-    "Epv2": Epv
+    "Epv2": Function.Epv,
+    "EpvGem2": Function.EpvGem,
+    "D2": Function.D,
 }
